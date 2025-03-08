bafdd64622dc7972e44e16e5818d6f3d0d73ef17
alizahfayyaz22 <alizahfayyaz22@gmail.com>
2025-03-09 02:31:22 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,35 +1,4 @@
-# import streamlit as st
-# import pandas as pd 
-# st.title("unit converter app")
-# st.markdown("### this app converts the length , weight and time")
-# st.write("select the category and enter a value to get your results instantly") 
-# category =st.selectbox("choose a category",["Length","weight","height"])
-# def convert_units(category,value,unit):
-#     if category=="length":
-#         if unit=="kilometers to miles":
-#             return value*0.621371
-#         elif unit=="miles to kilmeters":
-#             return value/0.621371
-#         elif category=="weight":
-#          if unit ==" kilogram to pounds":
-#             return value*2.20462
-#         elif unit =="pounds to kilogram":
-#             return value/2.20462
-#     elif category =="time":
-#         if unit == "seconds to minutes":
-#             return value/60
-#         elif unit=="minutes to seconds":
-#             return value*60
-#         if category=="length":
-#             unit=st.selectbox("select conversiions" ,["kilometers to miles,miles to kilmeters"])
-#         elif category=="weight":
-#             unit=st.selectbox("select conversions",["kilogram to pounds,pounds to kilogram"])
-#         elif category=="time":
-#             unit=st.selectbox("select conversions",["seconds to minutes, minutes to seconds"])
-#             value=st.number_input("enter the value to convert")
-#             if st.button ("convert"):
-#                 result=convert_units(category,value,unit)
-#                 st.success("the result is {result:.2f}")
+
 import streamlit as st
 
 st.title(" :rocket: Unit Converter App")
